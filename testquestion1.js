5e664030b68ceebc0601db3ce3707efecf9f2046
Techical7 <chuayunjie.22@gmail.com>
2025-10-26 03:55:47 +0000

    done

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
+let bird
+let background, birdImg;
+let assets;
 
+function preload(){
+    birdImg = loadImage("assets/bird.png");
+    background = loadImage("assets/background.png");
+}
+
+function setup(){
+    new Canvas(400, 600);
+    bird = new Sprite(200, 300, 20, 20);
+    bird.img =birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let square;
 
+function setup(){
+    new Canvas(400, 800);
+
+    world.gravity.y = 10;
+    
+
+    square = new Sprite(200, 300, 40, 40);
+}
+
+function draw(){
+    background("grey");
+    if (mouse.presses()){
+        square.velocity.y=-5;
+    }
+}
